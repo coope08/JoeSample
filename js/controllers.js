3f9f789bfc0ed35dcb03cde3efecdc62d4a0c404
Courtney <coope08@yahoo.com>
2013-08-13 14:59:59 -0400

    changed === to ==

--- js/controllers.js
+++ js/controllers.js
@@ -131,7 +131,7 @@ function ChatterTemplateCtrl($scope) {
     {
     	//check for login
     	alert(client.sessionId);
-    	if(client.sessionId !== null)
+    	if(client.sessionId != null)
     	{
     		if(addPosts != true)
     		{
@@ -140,7 +140,7 @@ function ChatterTemplateCtrl($scope) {
     		}
     		alert("calling ajax in getFeed");
     		//call forcetk client and request feed-items for a record
-    		client.ajax('/v24.0/chatter/feeds/record/'+ $scope.recordID + '/feed-items',$scope.getFeedSuccessCallback, $scope.errorCallback, "GET");
+    		client.ajax('/v24.0/chatter/feeds/record/'+ $scope.recordId + '/feed-items',$scope.getFeedSuccessCallback, $scope.getFeedErrorCallback, "GET");
     	}
     	else
     	{
