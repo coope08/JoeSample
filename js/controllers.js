0c245f4bc75a556c9cb08ea4971d646904d0272f
Courtney <coope08@yahoo.com>
2013-08-13 22:31:38 -0400

    www

--- js/controllers.js
+++ js/controllers.js
@@ -202,7 +202,7 @@ function ChatterTemplateCtrl($scope) {
     		$scope.addFeedItem(newItem);
     	};
     	//digest everything so the model gets updated
-    	$scope.$digest();
+    	$scope.$apply(function () {$scope.alertMessage = "Loading feed items...";});
     	
     	alert("before clearFeed");
     	$scope.clearFeed();
